78c3fdc9b33a0a87f245d7dab12189e933507639
peterjurkovic <petojurkovic@gmail.com>
2015-06-21 22:25:47 +0100

    Dril 3 alpha

--- src/sk/peterjurkovic/dril/v2/constants/Api.java
+++ src/sk/peterjurkovic/dril/v2/constants/Api.java
@@ -2,7 +2,7 @@ package sk.peterjurkovic.dril.v2.constants;
 
 public class Api {
 	
-	public static final String URL = "http://192.168.1.6/api/v1";
+	public static final String URL = "http://test.drilapp.com/v1";
 	
 	public static final String LOGIN = URL + "/user/login";
 	public static final String SYNC = URL + "/sync";
