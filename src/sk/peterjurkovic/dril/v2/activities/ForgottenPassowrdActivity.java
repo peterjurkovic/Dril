6ccd9a9d0e75cb9800ba911c259eff3216ce283e
peterjurkovic <petojurkovic@gmail.com>
2015-09-15 23:20:17 +0100

    Forgotten password impolemented

--- src/sk/peterjurkovic/dril/v2/activities/ForgottenPassowrdActivity.java
+++ src/sk/peterjurkovic/dril/v2/activities/ForgottenPassowrdActivity.java
@@ -1,17 +1,45 @@
 package sk.peterjurkovic.dril.v2.activities;
 
+import static sk.peterjurkovic.dril.utils.StringUtils.isBlank;
+
+import java.net.HttpURLConnection;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.Request.Method;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import sk.peterjurkovic.dril.AppController;
 import sk.peterjurkovic.dril.R;
+import sk.peterjurkovic.dril.db.DatabaseHelper;
+import sk.peterjurkovic.dril.utils.DeviceUtils;
+import sk.peterjurkovic.dril.utils.GoogleAnalyticsUtils;
+import sk.peterjurkovic.dril.utils.StringUtils;
+import sk.peterjurkovic.dril.utils.validation.EmailValidator;
+import sk.peterjurkovic.dril.v2.constants.Api;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ForgottenPassowrdActivity extends BaseActivity {
-
+	
+	private final static String TAG = RegistrationActivity.class.getSimpleName();
+	
 	private Button sendBtn;
 	private Button backBtn;
 	private EditText emailField;
+	private ProgressDialog pDialog;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +49,97 @@ public class ForgottenPassowrdActivity extends BaseActivity {
 		sendBtn = (Button) findViewById(R.id.send);
 		backBtn = (Button) findViewById(R.id.btnBackToLogin);
 		emailField = (EditText) findViewById(R.id.email);
+		pDialog = new ProgressDialog(context);
 		
+		sendBtn.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View view) {
+				if(!DeviceUtils.isDeviceOnline(context)){
+            		Toast.makeText(getApplicationContext(),R.string.err_internet_conn, Toast.LENGTH_LONG).show();
+            		return;
+            	}
+				final String email = emailField.getText().toString();
+				if(isBlank(email) || !EmailValidator.getInstance().isValid(email)){
+					Toast.makeText(context, R.string.err_email, Toast.LENGTH_SHORT).show();
+					return;
+				}
+				try {
+					sendRequest( email );
+				} catch (JSONException e) {
+					GoogleAnalyticsUtils.logException(e, context);
+				}
+				
+			}
+		});
 		
 		backBtn.setOnClickListener(new View.OnClickListener() {
-			 
-	            public void onClick(View view) {
-	                Intent i = new Intent(context,LoginActivity.class);
-	                startActivity(i);
-	                finish();
-	            }
-	        });
+			public void onClick(View view) {
+				Intent i = new Intent(context, LoginActivity.class);
+				startActivity(i);
+				finish();
+			}
+		});
+	}
+	
+	
+	
+	private void sendRequest(final String email) throws JSONException{
+		final JSONObject json = new JSONObject();
+		json.put("username", email);
+		json.put("deviceId", PreferenceManager.getDefaultSharedPreferences(context).getString(DatabaseHelper.DEVICE_ID, ""));
+		json.put("deviceName", DeviceUtils.getDeviceInfo());
+		
+		showDialog();
+        final JsonObjectRequest req = new JsonObjectRequest(Method.PUT, Api.FORGOTTEN_PASSOWRD, json,
+        	
+        		new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                    	hideDialog();
+                		showSuccessMessage(email);
+                    }
+                 },
+                 new Response.ErrorListener() {
+	                @Override
+	                public void onErrorResponse(VolleyError error) {
+	                	hideDialog();
+	                	NetworkResponse res = error.networkResponse;
+	                	if(res !=null && res.statusCode == HttpURLConnection.HTTP_BAD_REQUEST){
+	                			Toast.makeText(context, 
+	                					context.getString(R.string.email_not_found), 
+	                					Toast.LENGTH_LONG
+	                			).show();
+	                			return;
+	                	}
+	                	Toast.makeText(context, R.string.err_account_create, Toast.LENGTH_LONG).show();	                    
+	                }
+            });
+        
+        req.setRetryPolicy(new DefaultRetryPolicy(10000, 
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES, 
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(req, TAG);
+	}
+	
+	public void showSuccessMessage(String email){
+		TextView v = (TextView) findViewById(R.id.forrgottenPassDescr);
+		v.setText(context.getString(R.string.forgotten_password_changed, email));
+		sendBtn.setVisibility(View.GONE);
+		emailField.setVisibility(View.GONE);
+		((TextView) findViewById(R.id.yourEmailAddress))
+			.setVisibility(View.GONE);
+	}
+	
+	private void showDialog() {
+	    if (!pDialog.isShowing()){
+	        pDialog.show();
+	    }   
+	}
+	 
+	private void hideDialog() {
+	    if (pDialog.isShowing()){
+	        pDialog.dismiss();
+	    }
 	}
 }
--- src/sk/peterjurkovic/dril/v2/constants/Api.java
+++ src/sk/peterjurkovic/dril/v2/constants/Api.java
@@ -2,11 +2,15 @@ package sk.peterjurkovic.dril.v2.constants;
 
 public class Api {
 	
-	public static final String URL = "http://api.drilapp.com/v1";
+	//public static final String URL = "http://api.drilapp.com/v1";
+	public static final String URL = "http://192.168.1.9/api/v1";
 	
 	public static final String LOGIN = URL + "/user/login";
 	public static final String SYNC = URL + "/sync";
 	public static final String REGISTRATION = URL + "/users";
 	public static final String INIT_DRIL = URL + "/dril";
+	public static final String FORGOTTEN_PASSOWRD = URL + "/user/sendResetPassEmail";
+	
+
 
 }
