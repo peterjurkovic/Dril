bdc2d778dc9da8a0e13e05ed21f889eabab36ca5
peterjurkovic <petojurkovic@gmail.com>
2015-09-19 10:54:33 +0100

    api address changed

--- src/sk/peterjurkovic/dril/v2/constants/Api.java
+++ src/sk/peterjurkovic/dril/v2/constants/Api.java
@@ -2,8 +2,8 @@ package sk.peterjurkovic.dril.v2.constants;
 
 public class Api {
 	
-	//public static final String URL = "http://api.drilapp.com/v1";
-	public static final String URL = "http://192.168.1.9/api/v1";
+	public static final String URL = "http://api.drilapp.com/v1";
+	//public static final String URL = "http://192.168.1.9/api/v1";
 	
 	public static final String LOGIN = URL + "/user/login";
 	public static final String SYNC = URL + "/sync";
